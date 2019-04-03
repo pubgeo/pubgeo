5a80854a65be5817517b1fd09a776c7bfa90aaa2
Kevin Foster <kevin.foster@jhuapl.edu>
2019-04-03 11:34:48 -0400

    Added per-file licensing information; closes issue #27

--- src/align3d/align3d.cpp
+++ src/align3d/align3d.cpp
@@ -1,5 +1,33 @@
-// Copyright 2017 The Johns Hopkins University Applied Physics Laboratory.
-// Licensed under the MIT License. See LICENSE.txt in the project root for full license information.
+// Copyright (c) 2017-2019, The Johns Hopkins University /
+// Applied Physics Laboratory (JHU/APL)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Please reference the following when reporting any results using this software:
+//
+// M. Bosch, A. Leichtman, D. Chilcott, H. Goldberg, M. Brown, “Metric
+// Evaluation Pipeline for 3D Modeling of Urban Scenes,” ISPRS Archives, 2017.
+//
+// S. Almes, S. Hagstrom, D. Chilcott, H. Goldberg, M. Brown, “Open Source
+// Geospatial Tools to Enable Large Scale 3D Scene Modeling,” FOSS4G, 2017.
+//
+// For more information, please see: http://www.jhuapl.edu/pubgeo.html
 
 #include <stdlib.h>
 #include <iostream>
--- src/align3d/plugin.cpp
+++ src/align3d/plugin.cpp
@@ -1,4 +1,6 @@
-// Copyright (c) 2017, Bradley J Chambers (brad.chambers@gmail.com)
+// Original work Copyright (c) 2017, Bradley J Chambers (brad.chambers@gmail.com)
+// Modified work Copyright (c) 2018-2019, The Johns Hopkins University /
+//   Applied Physics Laboratory (JHU/APL)
 // All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
